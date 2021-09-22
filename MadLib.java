2010e398095ae51d9f6cfa6c69fb1f059201925b
sgarrett0 <sgarrett@salesianum.org>
2021-09-22 15:09:16 -0400

    added example/instruction

--- MadLib.java
+++ MadLib.java
@@ -4,13 +4,34 @@ public class MadLib{
     public static void main(String[] args){
         //Getting the code ready to listen for keyboard input.
         Scanner keyboardInput = new Scanner(System.in);
+        String finalTemplate = "";
+        
+        /**
+         * I want to change "I" in the sentence below.
+         * I can use substring to get a new string and replace word by word.
+         *  */  
+        String sentence0 = "I knows, but I never got to say what I needed to say to you.";
+        /**
+         * Sentence after I:
+         * Make sure you include the whitespace in the substring
+         *  */ 
+        String firstReplacement = sentence0.substring(1, sentence0.length()-1);
+        
+        //print statement to ask user for specific input
+        System.out.print("Please enter a singular noun:");
+        
+        //Asking/Storing keyboard input
+        String myInput = keyboardInput.nextLine();
+       
+        /**
+         * Concatenating my keyboard input and the rest of my sentence +
+         * Concatenating it all into my finalTemplate Variable that I will output
+         * at the end to show my altered quote
+         */
+        finalTemplate += myInput + firstReplacement;
 
-        System.out.print("please enter hello: ");
-        //Asking for keyboard input
-        String sayingHi = keyboardInput.nextLine();
-        System.out.print("Please enter your name: ");
-        String secondSentence = keyboardInput.nextLine();
-        System.out.print(sayingHi + " " + secondSentence);
+        //Outputting my altered quote
+        System.out.print(finalTemplate);
         keyboardInput.close();
     }
 
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MadLibs
-Creating a MadLib using the String Class
+Creating a MadLib using the String Class:
+
+Build a MadLibs using the String class. You will be able to accept keyboard input, store information, and build up your prompt to match your template with a few changes made to it. For my template I will be using: 
+
+"I know, but I never got to say what I needed to say to you. I'm sorry. You blame me for who you are. And I'm sorry. I'm sorry that I turned you. I'm sorry that you didn't get to die as a forgotten nobody on a bloody battlefield, and your father didn't get to live another day to be disgraced by you. Damon, I'm sorry that I gave your life purpose. Passion. Drive. Desire. I'm sorry that you are who you are because I'm the one that taught you how to love."
+
+I will be taking out some nouns, verbs, and adjectives to be replaced with my code. In order to replace these specific pieces, I will store my quote in chunked variables and use the substring(int from, int to) method to break it up, replace the missing piece and concatenate it back together. Using the length() method will be useful in getting the length of strings to properly find the substring. (This way you don't have to count everything) As you concatenate your smaller phrases back together, make sure you keep track of the total changed quote to be outputted at the end. I'll include an example in the code when it is released.
